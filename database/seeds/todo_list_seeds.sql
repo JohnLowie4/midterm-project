26456d9f05bfef8c4988951f05400cee9b94c0e2
John <jzlowie@gmail.com>
2021-06-26 18:31:51 -0600

    Database set up and initial commit, feel free to add your own data

---
+++ database/seeds/todo_list_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO todo_lists (user_id, title, category, description, status)
+VALUES (1, 'shop for fidget spinners', 'toBuy', 'me like the fidgety toy', TRUE),
+(1, 'watch Game of Thrones', 'toWatch', 'Cersei Lannister did not deserve a peaceful death', TRUE);
+INSERT INTO todo_lists (user_id, title, category, status)
+VALUES (1, 'read LHL Job Seekers Agreement', 'toRead', 'True');
